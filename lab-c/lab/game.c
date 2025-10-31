662fe98607458893f2fc49c49d207283ccaba386
Vinicius <viniciusmeloandre@gmail.com>
2025-10-30 21:41:30 -0300

    feat: add documentation for game.c and improve input validation

--- lab-c/lab/game.c
+++ lab-c/lab/game.c
@@ -1,36 +1,34 @@
-// objetivo: criar um jogo da velha
+// Jogo da Velha (Tic-Tac-Toe) - Versão usando apenas scanf
+// Dois jogadores se alternam para jogar no mesmo computador
 
 #include <stdio.h>
 #include <windows.h>
 #include <stdlib.h>
 
+// Função que realiza a jogada do jogador
 int play(char p, char table[3][3])
 {
    int l, c;
-   char buffer[100];
-
    printf("Linha: ");
-   fgets(buffer, sizeof(buffer), stdin);
-   if (sscanf(buffer, "%d", &l) != 1)
+   if (scanf("%d", &l) != 1)
    {
-      printf("Entrada inválida!\n");
+      printf("Entrada invalida!\n");
+      fflush(stdin); 
       return 0;
    }
-
    printf("Coluna: ");
-   fgets(buffer, sizeof(buffer), stdin);
-   if (sscanf(buffer, "%d", &c) != 1)
+   if (scanf("%d", &c) != 1)
    {
-      printf("Entrada inválida!\n");
+      printf("Entrada invalida!\n");
+      fflush(stdin); 
       return 0;
    }
-
+   fflush(stdin);
    if (l <= 0 || l > 3 || c <= 0 || c > 3)
    {
-      printf("[    Posição invalida!   ]\n\n");
+      printf("Posicao invalida!\n\n");
       return 0;
    }
-
    if (table[l - 1][c - 1] == ' ')
    {
       system("cls");
@@ -39,7 +37,7 @@ int play(char p, char table[3][3])
    }
    else
    {
-      printf("[   Posição já ocupada!  ]\n\n");
+      printf("Posicao ja ocupada!\n");
       return 0;
    }
 }
@@ -50,21 +48,13 @@ int wins(char table[3][3])
    {
       if (table[i][0] != ' ' && table[i][0] == table[i][1] && table[i][1] == table[i][2])
          return 1;
-
       if (table[0][i] != ' ' && table[0][i] == table[1][i] && table[1][i] == table[2][i])
          return 1;
    }
-
    if (table[0][0] != ' ' && table[0][0] == table[1][1] && table[1][1] == table[2][2])
-   {
       return 1;
-   }
-
    if (table[0][2] != ' ' && table[0][2] == table[1][1] && table[1][1] == table[2][0])
-   {
       return 1;
-   }
-
    return 0;
 }
 
@@ -85,42 +75,46 @@ void render(char table[3][3], char player)
 
 int main()
 {
-   SetConsoleOutputCP(65001);
-   char table[3][3] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
-
-   char player = 'O';
-   int movs = 0;
-
-   system("cls");
-
-   while (1)
+   int playing = 1;
+   while (playing)
    {
-      render(table, player);
-      if (play(player, table))
-      {  
-         if (wins(table))
+      char table[3][3] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+      char player = 'O';
+      int movs = 0;
+      system("cls");
+      while (1)
+      {
+         render(table, player);
+         if (play(player, table))
          {
-            render(table, player);
-            printf("Vitória do jogador: %c", player);
-            break;
-         }
-         movs++;
-         if (movs == 9)
-         {
-            render(table, player);
-            printf("Deu velha!");
-            break;
-         }
-         if (player == 'O')
-         {
-            player = 'X';
-         }
-         else
-         {
-            player = 'O';
+            if (wins(table))
+            {
+               render(table, player);
+               printf("Vitoria do jogador: %c\n", player);
+               break;
+            }
+            movs++;
+            if (movs == 9)
+            {
+               render(table, player);
+               printf("Deu velha!\n");
+               break;
+            }
+            player = (player == 'O') ? 'X' : 'O';
          }
       }
+      int opcao = 0;
+      printf("\n[1] Jogar novamente\n[2] Sair\nEscolha uma opcao: ");
+      if (scanf("%d", &opcao) != 1)
+      {
+         opcao = 2;
+      }
+      fflush(stdin); 
+      if (opcao != 1)
+      {
+         playing = 0;
+         printf("Saindo...\n");
+      }
    }
-
    return 0;
 }
